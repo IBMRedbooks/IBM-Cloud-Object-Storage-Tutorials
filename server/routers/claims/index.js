81c26e55a526c919b25d3ed5a9d0d59e4f10293e
Daniel Pittner <dpittner@de.ibm.com>
2018-05-10 18:45:07 -0500

    fix:lint

--- server/routers/claims/index.js
+++ server/routers/claims/index.js
@@ -1,25 +1,24 @@
 const express = require("express");
-const serviceManager = require("../../services/service-manager");
 
 module.exports = function(app) {
-  const router = express.Router();
+	const router = express.Router();
 
 
-     //POST a dummy claim back to the client
+	//POST a dummy claim back to the client
 
-      router.post("/", function(req, res) {
-                        res.setHeader("Location", `/claim/ComingInModule5`);
-                        res.status(201).end();
-        });     
+	router.post("/", function(req, res) {
+		res.setHeader("Location", "/claim/ComingInModule5");
+		res.status(201).end();
+	});
 
-        // GET a list of claims
-        router.get("/", function (req, res) {
-   			var claims = {'claims': 'ComingInModule5'};
-                        res.json(JSON.stringify(claims));
-        });
+	// GET a list of claims
+	router.get("/", function (req, res) {
+		const claims = {"claims": "ComingInModule5"};
+		res.json(JSON.stringify(claims));
+	});
 
 
-       app.use("/claim",
-                router);
+	app.use("/claim",
+		router);
 
 };
